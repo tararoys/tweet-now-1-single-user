0bb77fae0685df6dae8a7184191d77ab760e1dd1
Tara Roys <tlroys@gmail.com>
2014-02-11 13:45:18 -0600

    Added submit handler to application .js
    Added jquery submit handler in application.js. This is so that I can handle the tweet submission and page redirect 'asynchronously.'
    
    Todo: create an analogy for asynchronous and ajax.  Ajax is like...

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,4 +6,9 @@ end
 post '/tweet' do
   params[:tweet]
   CLIENT.update(params[:tweet])
+  redirect "/thank_you?tweet=#{params[:tweet]}"
+end
+
+get "/thank_you" do 
+  @tweet = params[:tweet]
 end
--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,9 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  
+  $( "#tweet" ).submit(function( event ) {
+    alert( "Handler for .submit() called." );
+    event.preventDefault();
+  });
 });
