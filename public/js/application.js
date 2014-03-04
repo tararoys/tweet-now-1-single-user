6a15f3bd775ed9f8423704cc5870a412175d9b62
Tara Roys <tlroys@gmail.com>
2014-03-04 11:34:26 -0600

    How do I show that I have sent a message to twitter?
    Use an!  An ajax request is like sending a package in the mail and still being able to do work because you don't have to wait for the package to come back to work on something else.
    
    A callback function is like a secretary giving you a message telling you that you got a reply to the letter you sent three weeks ago.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
+
+
+  $( "#tweet" ).submit(function( event ) {
+    event.preventDefault();
+    console.log("doing it");
+
+     $.ajax({
+      url : "/tweet",
+      type: "POST",
+      data : $("#tweet").serialize(),
+      success: function(msg)
+      { 
+          $("#reply").text(msg);
+          console.log(msg);
+          document.forms["tweet"].reset();
+      }
+
+    });
+  });
+
+
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
